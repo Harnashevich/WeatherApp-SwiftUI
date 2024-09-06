362a33879457a46c63f0cc1546746a35ccdc6f67
Andrei <garnash1313@gmail.com>
2024-09-06 13:16:53 +0300

    LocationManager

--- WeatherAppSwiftUI/LocationManager.swift
+++ WeatherAppSwiftUI/LocationManager.swift
@@ -10,27 +10,29 @@ import CoreLocation
 
 final class LocationManager: NSObject, CLLocationManagerDelegate {
     static let shared = LocationManager()
-
+    
     let manager = CLLocationManager()
-
+    
     var completion: (((lon: Double, lat: Double)) -> Void)?
-
+    
     func getLocation(completion: (((lon: Double, lat: Double)) -> Void)?) {
         self.completion = completion
         manager.requestWhenInUseAuthorization()
         manager.delegate = self
         manager.startUpdatingLocation()
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.first else {
             return
         }
         manager.stopUpdatingLocation()
-        completion?((lon: location.coordinate.longitude,
-                     lat: location.coordinate.latitude))
+        completion?((
+            lon: location.coordinate.longitude,
+            lat: location.coordinate.latitude
+        ))
     }
-
+    
     func resolveName(
         for location: CLLocation,
         completion: @escaping (String?) -> Void
@@ -40,13 +42,13 @@ final class LocationManager: NSObject, CLLocationManagerDelegate {
             guard let place = places?.first, error == nil else {
                 return
             }
-
+            
             var name = ""
-
+            
             if let city = place.locality {
                 name = city
             }
-
+            
             if let region = place.administrativeArea {
                 name += ", \(region)"
             }
